0d1532af92922a0ac6fbe28f94ca6684ad1afd5a
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-12-29 17:51:25 +0530

    a

--- src/pages/InterviewExperience/index.jsx
+++ src/pages/InterviewExperience/index.jsx
@@ -227,7 +227,7 @@ const InterviewExperiences = () => {
     }
     
     try {
-      const res = await fetch("http://localhost:5000/api/interview-experiences", {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/interview-experiences`, {
         method: "POST",
         credentials: "include",
         headers: {
